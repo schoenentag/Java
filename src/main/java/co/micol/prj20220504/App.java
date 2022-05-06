4795fcf6c3cb301d6a97aef8995a4bd0c314a1b0
Schokolade <siny9713@gmail.com>
2022-05-06 13:36:12 +0900

    Add files via upload
    20220504 Maven.java

---
+++ src/main/java/co/micol/prj20220504/App.java
@@ -0,0 +1,27 @@
+package co.micol.prj20220504;
+
+import java.sql.Date;
+
+import co.micol.prj20220504.dto.StudentVO;
+import co.micol.prj20220504.menu.Menu;
+
+public class App {
+	public static void main(String[] args) {
+		
+		Menu menu = new Menu();
+		menu.run();
+//		
+//		StudentVO student = new StudentVO();
+//		student.setId("park@abc.com");
+//		student.setName("박승리");
+//		student.setMajor("컴퓨터공학과");
+//		student.setBirthday(Date.valueOf("2000-01-01")); 
+//		student.setTel("010-1111-1111");
+//		student.setAddress("대구광역시 중구 종로");
+//		student.toString();
+//		
+//		student = new StudentVO("lee@park","이기자","경영학과","서울특별시","010-2222-2222",Date.valueOf("2000-04-01"));
+//		student.toString();
+		
+	}
+}
